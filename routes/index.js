caa1203a15f799c149932beea170a7d483013894
opteacher <opteacher@hotmail.com>
2017-10-29 01:51:20 +0800

    修改反向代理IP

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ proxy.on("error", (err, req, res, next) => {
 router.get("/", (req, res, next) => {
     switch (req.query.pto) {
         case "mon":
-            proxy.web(req, res, { target: "http://127.0.0.1:4000" });
+            proxy.web(req, res, { target: "http://localhost:4000" });
             break;
         default:
             res.render("index", {title: "Express"});
