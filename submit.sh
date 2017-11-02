4cc01c8117735017885ee82f15c6f3e08a93320b
opteacher <opteacher@hotmail.com>
2017-11-02 15:06:51 +0800

    删除.sh

--- submit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-read -p "输入提交说明：" cmtDsc
-git add .
-git commit -m $cmtDsc
-git push origin -u master
-scp -rp config/* root@101.132.121.47:/web-projects/ophome-service/oh-root/config/
